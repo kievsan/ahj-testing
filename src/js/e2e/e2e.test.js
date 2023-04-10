42965b94ea9e5a83cf8718e78af65b0e73d8c817
Sergey Kievskiy <knhel7@mail.ru>
2023-04-10 19:46:25 +0300

    Init commit

---
+++ src/js/e2e/e2e.test.js
@@ -0,0 +1,51 @@
+import puppetteer from "puppeteer";
+import { fork } from "child_process";
+
+jest.setTimeout(60000);
+describe("test validator form", () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = "http://localhost:8888";
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on("error", reject);
+      server.on("message", (message) => {
+        if (message === "ok") {
+          resolve();
+        }
+      });
+    });
+    browser = await puppetteer.launch({
+      // headless: false, // show gui
+      // slowMo: 100,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test("visa", async () => {
+    await page.goto(baseUrl);
+    const input = await page.$("#numberCard-input");
+    await input.type("4539283476916568");
+    const submit = await page.$("#card-submit");
+    await submit.click();
+    await page.waitForSelector(".cardVisa.active");
+  });
+
+  test("AmericanExpress", async () => {
+    await page.goto(baseUrl);
+    const input = await page.$("#numberCard-input");
+    await input.type("340054986290712");
+    const submit = await page.$("#card-submit");
+    await submit.click();
+    await page.waitForSelector(".cardAmericanExpress.active");
+  });
+});
